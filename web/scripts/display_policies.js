57ff4f31a78622cdf1dcddc597f127a042ea289f
Ruturaj Kadikar <ruturaj@infracloud.io>
2020-05-31 23:50:57 +0530

    refactoring

--- web/scripts/display_policies.js
+++ web/scripts/display_policies.js
@@ -183,29 +183,27 @@ function drawArrow(e){
     // target is the workload on which the network policy will be applied
     let source_label = document.getElementById(sourceid).title
     let target_label = document.getElementById(targetid).title;
-
     target_label = target_label.substring(1, target_label.length)
     source_label = target_label.substring(0, target_label.length)
-
+    // Converting to object
     target_label_split = target_label.split(",")
     let targetObj = {};
     for (i=0; i<target_label_split.length; i++) {
         val = target_label_split[i].split(":")
         targetObj[val[0]] = val[1]
     };
-
     source_label_split = source_label.split(",")
     let sourceObj = {};
     for (i=0; i<source_label_split.length; i++) {
         val = source_label_split[i].split(":")
         sourceObj[val[0]] = val[1]
     };
-
+    // variables
     var port = "8080";
     var policy_types = "Ingress"
     var protocol = "TCP"
     var policy_name = "test-network-policy"
-
+    // Substituting labels to form yaml
     var doc = jsyaml.load('apiVersion: networking.k8s.io\/v1\r\nkind: NetworkPolicy\r\nmetadata:\r\n  name: test-network-policy\r\n  namespace: default\r\nspec:\r\n  podSelector:\r\n    matchLabels:\r\n      role: db\r\n  policyTypes:\r\n  - Ingress\r\n  ingress:\r\n  - from:\r\n    - namespaceSelector:\r\n        matchLabels:\r\n          project: myproject\r\n    - podSelector:\r\n        matchLabels:\r\n          role: frontend\r\n    ports:\r\n    - protocol: TCP\r\n      port: 6379');
     doc.spec.podSelector.matchLabels = targetObj;
     doc.spec.ingress[0].from[1].podSelector.matchLabels = sourceObj;
